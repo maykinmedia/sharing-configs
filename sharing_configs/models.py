64552ee5e1d74f138aa0d67df2bea23196b3af77
Joeri Bekker <joeri@maykinmedia.nl>
2022-06-16 13:10:54 +0200

    :pencil2: Updated field labels.

--- sharing_configs/models.py
+++ sharing_configs/models.py
@@ -10,26 +10,31 @@ class SharingConfigsConfig(SingletonModel):
     """
 
     api_endpoint = models.URLField(
-        _("api_endpoint"),
+        _("API endpoint"),
         max_length=250,
-        help_text=_("Path to API point"),
+        help_text=_("Path to API point. For example: https://www.example.com/api/v1/"),
     )
     api_key = models.CharField(
-        _("api_key"),
+        _("API key"),
         max_length=128,
         help_text=_("API key for authorization"),
     )
     label = models.CharField(
         _("label"),
         max_length=50,
-        help_text=_("Label"),
+        help_text=_("This label should match the label in the Sharing Configs API."),
     )
     default_organisation = models.CharField(
         _("default organisation"),
         max_length=100,
         blank=True,
-        help_text=_("Default organisation"),
+        help_text=_(
+            "The default organisation to use in the description when sharing configurations."
+        ),
     )
 
     class Meta:
-        verbose_name = _("Sharing configuration config")
+        verbose_name = _("Sharing Configs configuration")
+
+    def __str__(self):
+        return self.label
