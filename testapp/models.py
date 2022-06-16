b2b20cd87d7e5243e23894181c822849dbfc1c30
Joeri Bekker <joeri@maykinmedia.nl>
2022-06-16 13:10:22 +0200

    :card_file_box: Better labels and allow no theme in testapp

--- testapp/models.py
+++ testapp/models.py
@@ -9,17 +9,17 @@ class Configuration(SingletonModel):
     configuration that holds the current theme
     """
 
-    theme = models.ForeignKey("Theme", on_delete=models.CASCADE, null=True)
+    theme = models.ForeignKey("Theme", on_delete=models.SET_NULL, null=True, blank=True)
 
 
 class Theme(models.Model):
     """All attributes used for theming."""
 
-    name = models.CharField("name", max_length=120)
-    primary = models.CharField("primary", max_length=60)
-    secondary = models.CharField("secondary", max_length=60)
-    accent = models.CharField("accent", max_length=60)
-    primary_fg = models.CharField("primary_fg", max_length=60)
+    name = models.CharField("name", max_length=100)
+    primary = models.CharField("primary color", max_length=7)
+    secondary = models.CharField("secondary color", max_length=7)
+    accent = models.CharField("accent color", max_length=7)
+    primary_fg = models.CharField("primary foreground color", max_length=7)
 
     def __str__(self) -> str:
         return self.name
